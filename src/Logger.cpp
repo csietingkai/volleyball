5bd604eda3dbc23561d7e4372f291cf8bdba21f3
RBRBRB <qaz8899921@gmail.com>
2018-11-11 20:43:27 +0800

    set filter level and sample output

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,5 +1,40 @@
 #include "Logger.h"
+namespace logging = boost::log;
 
 Logger::Logger(const string class_name)
 {
+    this->log_message = class_name;
+}
+void Logger::init_logging()
+{
+    //logging::add_file_log("sample.log");
+
+    logging::core::get()->set_filter
+    (
+        logging::trivial::severity >= logging::trivial::trace
+    );
+}
+void Logger::trace(const string message)
+{
+    BOOST_LOG_TRIVIAL(trace) << message;
+}
+void Logger::debug(const string message)
+{
+    BOOST_LOG_TRIVIAL(debug) << message;
+}
+void Logger::info(const string message)
+{
+    BOOST_LOG_TRIVIAL(info) << message;
+}
+void Logger::warning(const string message)
+{
+    BOOST_LOG_TRIVIAL(warning) << message;
+}
+void Logger::error(const string message)
+{
+    BOOST_LOG_TRIVIAL(error) << message;
+}
+void Logger::fatal(const string message)
+{
+    BOOST_LOG_TRIVIAL(fatal) << message;
 }
