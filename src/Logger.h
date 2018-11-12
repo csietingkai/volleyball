4b0a594e98432b5df309e69f5c88944f164d4e46
RBRBRB <qaz8899921@gmail.com>
2018-11-12 11:45:35 +0800

    add console.hpp

--- src/Logger.h
+++ src/Logger.h
@@ -8,6 +8,7 @@
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
 
 using namespace std;
 // add namespace you need
