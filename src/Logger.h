7c54ea2b3703142a3612329d3a6a5841c1c35e64
tingkai <tingkai@csie.io>
2018-11-13 20:35:11 +0800

    try to fix travis-ci null_deleter error which not happaned in client

--- src/Logger.h
+++ src/Logger.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <boost/core/null_deleter.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
@@ -17,9 +18,9 @@
 #include <boost/log/sinks/sync_frontend.hpp>
 #include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/support/date_time.hpp>
 #include <boost/smart_ptr/shared_ptr.hpp>
 #include <boost/smart_ptr/make_shared_object.hpp>
-#include <boost/log/support/date_time.hpp>
 
 using namespace std;
 // add namespace you need
