e0818fe675309c891ffe9b8e548ceefb63066a24
RBRBRB <qaz8899921@gmail.com>
2018-11-13 16:58:59 +0800

    update private function

--- src/Logger.h
+++ src/Logger.h
@@ -3,12 +3,22 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/smart_ptr/make_shared_object.hpp>
 
 using namespace std;
 // add namespace you need
@@ -31,8 +41,7 @@ class Logger
 	private:
 		// add any functions and variables you need
 		void init_logging();
-		void update_logging_low();
-		void update_logging_up();
+		void init_logfile();
 		string log_message;
 };
 
