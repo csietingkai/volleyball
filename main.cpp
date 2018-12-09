7c413065d56192b62ff549ff955be5e25146f60e
tingkai <tingkai@csie.io>
2018-12-09 09:42:58 +0800

    add MainWindowView example

--- main.cpp
+++ main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 
-/*#include "src/common.h"
-#include "src/Datee.h"
-#include "src/DateeTime.h"
-#include "src/MainWindowView.h"
-#include "src/MySQLConnector.h"
-#include "src/Person.h"
-#include "src/Schedule.h"
-#include "src/Team.h"
-#include "src/Time.h"
-#include "src/Logger.h" 
-#include "test/test.h"*/
 #include "src/beans/DateTime.h"
 #include "src/tools/SqlCommandBuilder.h"
 #include "src/utils/ServerInfo.h"
 #include "src/utils/Utils.h"
+#include "src/views/MainWindowView.h"
 #include "test/test.h"
 
 int main(int argc, char* argv[])
@@ -27,11 +17,11 @@ int main(int argc, char* argv[])
 	else
 	{
 		// TODO need to test manually
-		/*Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
+		Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
 
-		MainWindowView window;
+		voba::MainWindowView window;
 
-		return app->run(window);*/
+		return app->run(window);
 		return 0;
 	}
 }
