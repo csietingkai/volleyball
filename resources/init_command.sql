847bd1a7e20cbb286cb1924e2011d98c58bbfab6
tingkai <tingkai@csie.io>
2018-12-16 22:13:36 +0800

    remove foreign key

--- resources/init_command.sql
+++ resources/init_command.sql
@@ -22,8 +22,7 @@ CREATE TABLE IF NOT EXISTS teams
 	MEMBER_ID varchar(255) NOT NULL,
 	PREFER_WEEK varchar(255) NOT NULL,
 	PREFER_TIME varchar(255) NOT NULL,
-	PRIMARY KEY(ID, MEMBER_ID),
-	FOREIGN KEY(MEMBER_ID) REFERENCES persons(ID)
+	PRIMARY KEY(ID, MEMBER_ID)
 );
 
 CREATE TABLE IF NOT EXISTS games
@@ -32,7 +31,19 @@ CREATE TABLE IF NOT EXISTS games
 	TEAM_1_ID varchar(255) NOT NULL, 
 	TEAM_2_ID varchar(255) NOT NULL,
 	DATETIME DATETIME,
-	PRIMARY KEY(ID),
-	FOREIGN KEY(TEAM_1_ID) REFERENCES teams(ID),
-	FOREIGN KEY(TEAM_2_ID) REFERENCES teams(ID)
+	PRIMARY KEY(ID)
 );
+
+INSERT INTO `persons` (`ID`, `NAME`, `AGE`, `GENDER`, `PHONE_NUMBER`, `IS_ACTIVE`) VALUES
+('353de133f85ff64454a58ff782c31f483854c8f5', 'test1', 21, b'1', '0987654321', b'1'),
+('4e9dac67d5863be963d80f64925a689a68c8cbf7', 'test4', 24, b'1', '0987654321', b'1'),
+('5b638035021f1dffae1dc5be45f490534edd628e', 'test3', 23, b'1', '0987654321', b'1'),
+('6bced46dd43429b141c07d427c868a4002e104f5', 'test2', 22, b'1', '0987654321', b'1'),
+('a2259774926c8c9b3a60d5d2cd7db9854e94b4d3', 'test5', 25, b'1', '0987654321', b'1');
+
+INSERT INTO `teams` (`ID`, `NAME`, `MEMBER_ID`, `PREFER_WEEK`, `PREFER_TIME`) VALUES
+('e8177aaa852a9537e65234121593fbf649fc4a92', 'test_team', '353de133f85ff64454a58ff782c31f483854c8f5', 'true;false;false;false;false;false;false', '19:00:00'),
+('e8177aaa852a9537e65234121593fbf649fc4a92', 'test_team', '4e9dac67d5863be963d80f64925a689a68c8cbf7', 'true;false;false;false;false;false;false', '19:00:00'),
+('e8177aaa852a9537e65234121593fbf649fc4a92', 'test_team', '5b638035021f1dffae1dc5be45f490534edd628e', 'true;false;false;false;false;false;false', '19:00:00'),
+('e8177aaa852a9537e65234121593fbf649fc4a92', 'test_team', '6bced46dd43429b141c07d427c868a4002e104f5', 'true;false;false;false;false;false;false', '19:00:00'),
+('e8177aaa852a9537e65234121593fbf649fc4a92', 'test_team', 'a2259774926c8c9b3a60d5d2cd7db9854e94b4d3', 'true;false;false;false;false;false;false', '19:00:00');
